bb8caa3d81f9de2aef2d169afbb9613ae54e75bb
andradealmeida20 <gustavo.almeida@psicologiaviva.com.br>
2022-03-30 20:12:04 -0300

    aula 05 selenium driver

---
+++ src/test/java/br/com/chronosAcademy/automacaoWeb/TesteWeb.java
@@ -0,0 +1,28 @@
+package br.com.chronosAcademy.automacaoWeb;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import static org.junit.Assert.*;
+
+
+public class TesteWeb {
+
+    @Test
+    public void primeiroTeste(){
+        WebDriverManager.chromedriver().setup();
+        ChromeDriver driver;
+
+        driver = new ChromeDriver(); //abre uma nova instância(página) do navegador
+        driver.manage().window().maximize(); //maximiniza a janela do navegador
+        driver.get("https://www.chronosacademy.com.br");
+        String xpathTitulo = "/html/body/div/div/div/div/div/section[2]/div[3]/div/div/div/div/div[1]/div/h4";
+        WebElement txtTitulo = driver.findElement(By.xpath(xpathTitulo));
+        String titulo = txtTitulo.getText();
+        assertEquals("Porque Tempo É Conhecimento", titulo);
+
+        driver.quit();
+    }
+}
