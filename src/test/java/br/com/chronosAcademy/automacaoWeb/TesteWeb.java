4c5ee9ee3a2e64d68cdf7c21d62cb39f9f801638
andradealmeida20 <gustavo.almeida@psicologiaviva.com.br>
2022-04-01 15:09:54 -0300

    aula 06 anotacoes

--- src/test/java/br/com/chronosAcademy/automacaoWeb/TesteWeb.java
+++ src/test/java/br/com/chronosAcademy/automacaoWeb/TesteWeb.java
@@ -1,6 +1,8 @@
 package br.com.chronosAcademy.automacaoWeb;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
@@ -10,19 +12,28 @@ import static org.junit.Assert.*;
 
 public class TesteWeb {
 
-    @Test
-    public void primeiroTeste(){
-        WebDriverManager.chromedriver().setup();
-        ChromeDriver driver;
+    ChromeDriver driver;
 
+    @Before
+    public void inicializaTeste(){
+        WebDriverManager.chromedriver().setup();
         driver = new ChromeDriver(); //abre uma nova instância(página) do navegador
         driver.manage().window().maximize(); //maximiniza a janela do navegador
         driver.get("https://www.chronosacademy.com.br");
+    }
+
+    @Test
+    public void primeiroTeste(){
+
         String xpathTitulo = "/html/body/div/div/div/div/div/section[2]/div[3]/div/div/div/div/div[1]/div/h4";
         WebElement txtTitulo = driver.findElement(By.xpath(xpathTitulo));
         String titulo = txtTitulo.getText();
-        assertEquals("Porque Tempo É Conhecimento", titulo);
+        assertEquals("Porque Tempo É Conhecimento", titulo); //compara o texto com o da página web
 
+    }
+
+    @After
+    public void finalizaTeste(){
         driver.quit();
     }
 }
