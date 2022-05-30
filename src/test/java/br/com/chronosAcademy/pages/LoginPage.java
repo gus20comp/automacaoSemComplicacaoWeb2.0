7c17cdf680688d508dabc2aafca3c041d51694b9
andradealmeida20 <gustavo.almeida@psicologiaviva.com.br>
2022-05-30 16:56:24 -0300

    aula 17 try catch e excecao

--- src/test/java/br/com/chronosAcademy/pages/LoginPage.java
+++ src/test/java/br/com/chronosAcademy/pages/LoginPage.java
@@ -13,6 +13,7 @@ public class LoginPage {
     }
 
     public void clickBtnLogin(){
+        Driver.visibilityOf(loginMap.btnLogin);
         loginMap.btnLogin.click();
     }
 
@@ -47,4 +48,14 @@ public class LoginPage {
     public boolean isBtnSignIn(){
         return loginMap.btnSignIn.isEnabled();
     }
+
+    public void visibilityOfBtnFechar(){
+        Driver.visibilityOf(loginMap.btnFechar);
+    }
+
+    public void invisibilityOfBtnFechar(){
+        Driver.invisibilityOf(loginMap.btnFechar);
+    }
+
+
 }
